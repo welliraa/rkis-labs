8a4e90105aff3c2df5f3116fbd5920169a21361d
welliraa <wellira235@gmail.com>
2025-06-23 23:20:23 +0300

    Add files via upload

---
+++ rkis-lab3/LoginWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using WPFLabs.Repository;
+
+namespace WPFLabs
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var email = EmailTextBox.Text;
+            var password = PasswordTextBox.Password;
+
+            try
+            {
+                UserRepository.GetInstance().Login(email, password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            new MainEmptyWindow().Show();
+            Close();
+        }
+
+        private void RegistrationWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new RegistrationWindow().Show();
+            Close();
+        }
+    }
+}
